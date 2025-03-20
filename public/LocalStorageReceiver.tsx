4c9382b6cb7d533c5c4ab0710429e62946d239bc
Cameron Fraser <cameron.fraser@alleninstitute.org>
2025-03-20 14:34:40 -0700

    write to local storage

--- public/LocalStorageReceiver.tsx
+++ public/LocalStorageReceiver.tsx
@@ -3,7 +3,12 @@ import React from "react";
 const LocalStorageReceiver: React.FC = () => {
   React.useLayoutEffect(() => {
     const receiveMessage = (e: MessageEvent) => {
-      console.log(e.data);
+      if (e.origin === window.location.origin) {
+        return;
+      }
+
+      window.localStorage.setItem("scenes", e.data);
+      (e.source as Window)?.postMessage("SUCCESS", e.origin);
     };
 
     window.addEventListener("message", receiveMessage);
