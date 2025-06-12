10ffbd524a7c5bca7950de98d40a464a83132886
Cameron Fraser <cameron.fraser@alleninstitute.org>
2025-06-11 23:08:32 -0700

    move most logic into that effect

--- src/aics-image-viewer/components/VolumeProvider.tsx
+++ src/aics-image-viewer/components/VolumeProvider.tsx
@@ -32,16 +32,16 @@ export type UseVolumeOptions = {
 };
 
 const useVolume = (scenePaths: (string | string[] | RawArrayLoaderOptions)[], options?: UseVolumeOptions): void => {
-  const viewerState = useContext(ViewerStateContext).ref.current;
-  const {
-    changeChannelSetting,
-    changeViewerSetting,
-    channelSettings,
-    getChannelsAwaitingResetOnLoad,
-    getCurrentViewerChannelSettings,
-    onResetChannel,
-    setChannelSettings,
-  } = viewerState;
+  const viewerStateRef = useContext(ViewerStateContext).ref;
+  // const {
+  //   changeChannelSetting,
+  //   changeViewerSetting,
+  //   channelSettings,
+  //   getChannelsAwaitingResetOnLoad,
+  //   getCurrentViewerChannelSettings,
+  //   onResetChannel,
+  //   setChannelSettings,
+  // } = viewerStateRef.current;
   const onError = options?.onError;
 
   const [image, setImage] = useState<Volume | null>(null);
@@ -66,177 +66,187 @@ const useVolume = (scenePaths: (string | string[] | RawArrayLoaderOptions)[], op
   // channel indexes, sorted by category
   const [channelGroupedByType, setChannelGroupedByType] = useState<ChannelGrouping>({});
 
-  const setAllChannelsUnloaded = (numberOfChannels: number): void => {
-    setChannelVersions(new Array(numberOfChannels).fill(0));
-  };
-
-  const setOneChannelLoaded = (index: number): void => {
-    const newVersions = channelVersionsRef.current.slice();
-    newVersions[index]++;
-    setChannelVersions(newVersions);
-  };
-
   const getOneChannelSetting = (channelName: string, settings?: ChannelState[]): ChannelState | undefined => {
-    return (settings || viewerState.channelSettings).find((channel) => channel.name === channelName);
-  };
-
-  /**
-   * Updates a channel's ramp and control points after new data has been loaded.
-   *
-   * Also handles initializing the ramp/control points on initial load and resetting
-   * them when the channel is reset.
-   */
-  const updateChannelTransferFunction = (
-    aimg: Volume,
-    thisChannelsSettings: ChannelState,
-    channelIndex: number
-  ): void => {
-    const thisChannel = aimg.getChannel(channelIndex);
-
-    // If this is the first load of this image, auto-generate initial LUTs
-    if (
-      !hasChannelLoadedRef.current[channelIndex] ||
-      !thisChannelsSettings.controlPoints ||
-      !thisChannelsSettings.ramp ||
-      getChannelsAwaitingResetOnLoad().has(channelIndex)
-    ) {
-      const viewerChannelSettings = getCurrentViewerChannelSettings();
-      const { ramp, controlPoints } = initializeLut(aimg, channelIndex, viewerChannelSettings);
-
-      changeChannelSetting(channelIndex, {
-        controlPoints: controlPoints,
-        ramp: controlPointsToRamp(ramp),
-        // set the default range of the transfer function editor to cover the full range of the data type
-        plotMax: DTYPE_RANGE[thisChannel.dtype].max,
-      });
-      onResetChannel(channelIndex);
-    } else {
-      // try not to update lut from here if we are in play mode
-      // if (playingAxis !== null) {
-      // do nothing here?
-      // tell gui that we have updated control pts?
-      //changeChannelSetting(channelIndex, "controlPoints", aimg.getChannel(channelIndex).lut.controlPoints);
-      // }
-      const oldRange = channelRangesRef.current[channelIndex];
-      if (thisChannelsSettings.useControlPoints) {
-        // control points were just automatically remapped - update in state
-        const rampControlPoints = rampToControlPoints(thisChannelsSettings.ramp);
-        // now manually remap ramp using the channel's old range
-        const remappedRampControlPoints = remapControlPointsForChannel(rampControlPoints, oldRange, thisChannel);
-        changeChannelSetting(channelIndex, {
-          ramp: controlPointsToRamp(remappedRampControlPoints),
-          controlPoints: thisChannel.lut.controlPoints,
-        });
-      } else {
-        // ramp was just automatically remapped - update in state
-        const ramp = controlPointsToRamp(thisChannel.lut.controlPoints);
-        // now manually remap control points using the channel's old range
-        const { controlPoints } = thisChannelsSettings;
-        const remappedControlPoints = remapControlPointsForChannel(controlPoints, oldRange, thisChannel);
-        changeChannelSetting(channelIndex, { controlPoints: remappedControlPoints, ramp: ramp });
-      }
-    }
-  };
-
-  const onChannelDataLoaded = (aimg: Volume, thisChannelsSettings: ChannelState, channelIndex: number): void => {
-    const thisChannel = aimg.getChannel(channelIndex);
-    updateChannelTransferFunction(aimg, thisChannelsSettings, channelIndex);
-
-    // save the channel's new range for remapping next time
-    channelRangesRef.current[channelIndex] = [thisChannel.rawMin, thisChannel.rawMax];
-
-    view3d.updateLuts(aimg);
-    view3d.onVolumeData(aimg, [channelIndex]);
-
-    view3d.setVolumeChannelEnabled(aimg, channelIndex, thisChannelsSettings.volumeEnabled);
-    if (aimg.channelNames[channelIndex] === getCurrentViewerChannelSettings()?.maskChannelName) {
-      view3d.setVolumeChannelAsMask(aimg, channelIndex);
-    }
-    hasChannelLoadedRef.current[channelIndex] = true;
-
-    // when any channel data has arrived:
-    setSendingQueryRequest(false);
-    setOneChannelLoaded(channelIndex);
-    if (aimg.isLoaded()) {
-      view3d.updateActiveChannels(aimg);
-      setImageLoaded(true);
-      playControls.onImageLoaded();
-    }
-  };
-
-  const setChannelStateForNewImage = (channelNames: string[]): ChannelState[] | undefined => {
-    const grouping = makeChannelIndexGrouping(channelNames, getCurrentViewerChannelSettings());
-    setChannelGroupedByType(grouping);
-
-    // compare each channel's new displayName to the old displayNames currently in state:
-    // same number of channels, and each channel has same displayName
-    const allNamesAreEqual = channelNames.every((name, idx) => {
-      const displayName = getDisplayName(name, idx, getCurrentViewerChannelSettings());
-      return displayName === channelSettings[idx]?.displayName;
-    });
-
-    if (allNamesAreEqual) {
-      const newChannelSettings = channelNames.map((channel, index) => {
-        return { ...channelSettings[index], name: channel };
-      });
-      setChannelSettings(newChannelSettings);
-      return newChannelSettings;
-    }
-
-    const newChannelSettings = channelNames.map((channel, index) => {
-      const color = getDefaultChannelColor(index);
-      return initializeOneChannelSetting(channel, index, color, getCurrentViewerChannelSettings());
-    });
-    setChannelSettings(newChannelSettings);
-    return newChannelSettings;
-  };
-
-  const placeImageInViewer = (aimg: Volume, newChannelSettings?: ChannelState[]): void => {
-    setImage(aimg);
-
-    const channelSetting = newChannelSettings || channelSettings;
-    view3d.removeAllVolumes();
-    view3d.addVolume(aimg, {
-      // Immediately passing down channel parameters isn't strictly necessary, but keeps things looking consistent on load
-      channels: aimg.channelNames.map((name) => {
-        const ch = getOneChannelSetting(name, channelSetting);
-        if (!ch) {
-          return {};
-        }
-        return {
-          enabled: ch.volumeEnabled,
-          isosurfaceEnabled: ch.isosurfaceEnabled,
-          isovalue: ch.isovalue,
-          isosurfaceOpacity: ch.opacity,
-          color: ch.color,
-        };
-      }),
-    });
-
-    const mode3d = viewerState.viewMode === ViewMode.threeD;
-    setIndicatorPositions(view3d, clippingPanelOpenRef.current, aimg.imageInfo.times > 1, numScenes > 1, mode3d);
-    // TODO remove
-    // imageLoadHandlers.current.forEach((effect) => effect(aimg));
-
-    playControls.stepAxis = (axis: AxisName | "t") => {
-      if (axis === "t") {
-        changeViewerSetting("time", (viewerState.time + 1) % aimg.imageInfo.times);
-      } else {
-        const max = aimg.imageInfo.volumeSize[axis];
-        const current = viewerState.slice[axis] * max;
-        changeViewerSetting("slice", { ...viewerState.slice, [axis]: ((current + 1) % max) / max });
-      }
-    };
-    playControls.getVolumeIsLoaded = aimg.isLoaded.bind(aimg);
-
-    view3d.updateActiveChannels(aimg);
-    // make sure we pick up whether the image needs to be in single-slice mode
-    view3d.setCameraMode(viewerState.viewMode);
+    return (settings || channelSettings).find((channel) => channel.name === channelName);
   };
 
   useEffect(() => {
+    const {
+      changeChannelSetting,
+      changeViewerSetting,
+      channelSettings,
+      getChannelsAwaitingResetOnLoad,
+      getCurrentViewerChannelSettings,
+      onResetChannel,
+      setChannelSettings,
+    } = viewerStateRef.current;
+
+    const setChannelStateForNewImage = (channelNames: string[]): ChannelState[] | undefined => {
+      const grouping = makeChannelIndexGrouping(channelNames, getCurrentViewerChannelSettings());
+      setChannelGroupedByType(grouping);
+
+      // compare each channel's new displayName to the old displayNames currently in state:
+      // same number of channels, and each channel has same displayName
+      const allNamesAreEqual = channelNames.every((name, idx) => {
+        const displayName = getDisplayName(name, idx, getCurrentViewerChannelSettings());
+        return displayName === channelSettings[idx]?.displayName;
+      });
+
+      if (allNamesAreEqual) {
+        const newChannelSettings = channelNames.map((channel, index) => {
+          return { ...channelSettings[index], name: channel };
+        });
+        setChannelSettings(newChannelSettings);
+        return newChannelSettings;
+      }
+
+      const newChannelSettings = channelNames.map((channel, index) => {
+        const color = getDefaultChannelColor(index);
+        return initializeOneChannelSetting(channel, index, color, getCurrentViewerChannelSettings());
+      });
+      setChannelSettings(newChannelSettings);
+      return newChannelSettings;
+    };
+
+    const placeImageInViewer = (aimg: Volume, newChannelSettings?: ChannelState[]): void => {
+      setImage(aimg);
+
+      const channelSetting = newChannelSettings || channelSettings;
+      view3d.removeAllVolumes();
+      view3d.addVolume(aimg, {
+        // Immediately passing down channel parameters isn't strictly necessary, but keeps things looking consistent on load
+        channels: aimg.channelNames.map((name) => {
+          const ch = getOneChannelSetting(name, channelSetting);
+          if (!ch) {
+            return {};
+          }
+          return {
+            enabled: ch.volumeEnabled,
+            isosurfaceEnabled: ch.isosurfaceEnabled,
+            isovalue: ch.isovalue,
+            isosurfaceOpacity: ch.opacity,
+            color: ch.color,
+          };
+        }),
+      });
+
+      const mode3d = viewerState.viewMode === ViewMode.threeD;
+      setIndicatorPositions(view3d, clippingPanelOpenRef.current, aimg.imageInfo.times > 1, numScenes > 1, mode3d);
+      // TODO remove
+      // imageLoadHandlers.current.forEach((effect) => effect(aimg));
+
+      playControls.stepAxis = (axis: AxisName | "t") => {
+        if (axis === "t") {
+          changeViewerSetting("time", (viewerState.time + 1) % aimg.imageInfo.times);
+        } else {
+          const max = aimg.imageInfo.volumeSize[axis];
+          const current = viewerState.slice[axis] * max;
+          changeViewerSetting("slice", { ...viewerState.slice, [axis]: ((current + 1) % max) / max });
+        }
+      };
+      playControls.getVolumeIsLoaded = aimg.isLoaded.bind(aimg);
+
+      view3d.updateActiveChannels(aimg);
+      // make sure we pick up whether the image needs to be in single-slice mode
+      view3d.setCameraMode(viewerState.viewMode);
+    };
+
+    const setAllChannelsUnloaded = (numberOfChannels: number): void => {
+      setChannelVersions(new Array(numberOfChannels).fill(0));
+    };
+
+    const setOneChannelLoaded = (index: number): void => {
+      const newVersions = channelVersionsRef.current.slice();
+      newVersions[index]++;
+      setChannelVersions(newVersions);
+    };
+
+    /**
+     * Updates a channel's ramp and control points after new data has been loaded.
+     *
+     * Also handles initializing the ramp/control points on initial load and resetting
+     * them when the channel is reset.
+     */
+    const updateChannelTransferFunction = (
+      aimg: Volume,
+      thisChannelsSettings: ChannelState,
+      channelIndex: number
+    ): void => {
+      const thisChannel = aimg.getChannel(channelIndex);
+
+      // If this is the first load of this image, auto-generate initial LUTs
+      if (
+        !hasChannelLoadedRef.current[channelIndex] ||
+        !thisChannelsSettings.controlPoints ||
+        !thisChannelsSettings.ramp ||
+        getChannelsAwaitingResetOnLoad().has(channelIndex)
+      ) {
+        const viewerChannelSettings = getCurrentViewerChannelSettings();
+        const { ramp, controlPoints } = initializeLut(aimg, channelIndex, viewerChannelSettings);
+
+        changeChannelSetting(channelIndex, {
+          controlPoints: controlPoints,
+          ramp: controlPointsToRamp(ramp),
+          // set the default range of the transfer function editor to cover the full range of the data type
+          plotMax: DTYPE_RANGE[thisChannel.dtype].max,
+        });
+        onResetChannel(channelIndex);
+      } else {
+        // try not to update lut from here if we are in play mode
+        // if (playingAxis !== null) {
+        // do nothing here?
+        // tell gui that we have updated control pts?
+        //changeChannelSetting(channelIndex, "controlPoints", aimg.getChannel(channelIndex).lut.controlPoints);
+        // }
+        const oldRange = channelRangesRef.current[channelIndex];
+        if (thisChannelsSettings.useControlPoints) {
+          // control points were just automatically remapped - update in state
+          const rampControlPoints = rampToControlPoints(thisChannelsSettings.ramp);
+          // now manually remap ramp using the channel's old range
+          const remappedRampControlPoints = remapControlPointsForChannel(rampControlPoints, oldRange, thisChannel);
+          changeChannelSetting(channelIndex, {
+            ramp: controlPointsToRamp(remappedRampControlPoints),
+            controlPoints: thisChannel.lut.controlPoints,
+          });
+        } else {
+          // ramp was just automatically remapped - update in state
+          const ramp = controlPointsToRamp(thisChannel.lut.controlPoints);
+          // now manually remap control points using the channel's old range
+          const { controlPoints } = thisChannelsSettings;
+          const remappedControlPoints = remapControlPointsForChannel(controlPoints, oldRange, thisChannel);
+          changeChannelSetting(channelIndex, { controlPoints: remappedControlPoints, ramp: ramp });
+        }
+      }
+    };
+
+    const onChannelDataLoaded = (aimg: Volume, thisChannelsSettings: ChannelState, channelIndex: number): void => {
+      const thisChannel = aimg.getChannel(channelIndex);
+      updateChannelTransferFunction(aimg, thisChannelsSettings, channelIndex);
+
+      // save the channel's new range for remapping next time
+      channelRangesRef.current[channelIndex] = [thisChannel.rawMin, thisChannel.rawMax];
+
+      view3d.updateLuts(aimg);
+      view3d.onVolumeData(aimg, [channelIndex]);
+
+      view3d.setVolumeChannelEnabled(aimg, channelIndex, thisChannelsSettings.volumeEnabled);
+      if (aimg.channelNames[channelIndex] === getCurrentViewerChannelSettings()?.maskChannelName) {
+        view3d.setVolumeChannelAsMask(aimg, channelIndex);
+      }
+      hasChannelLoadedRef.current[channelIndex] = true;
+
+      // when any channel data has arrived:
+      setSendingQueryRequest(false);
+      setOneChannelLoaded(channelIndex);
+      if (aimg.isLoaded()) {
+        view3d.updateActiveChannels(aimg);
+        setImageLoaded(true);
+        playControls.onImageLoaded();
+      }
+    };
+
     const openImage = async (): Promise<void> => {
-      const { channelSettings, scene, time } = viewerState;
+      const { channelSettings, scene, time } = viewerStateRef.current;
       setSendingQueryRequest(true);
       setImageLoaded(false);
       hasChannelLoadedRef.current = [];
@@ -294,8 +304,8 @@ const useVolume = (scenePaths: (string | string[] | RawArrayLoaderOptions)[], op
       // will be shown whenever we switch back to the same viewer settings (2D Z-axis view mode).
       // (We don't do this for ZX and YZ modes because we assume that the data won't be chunked along the
       // X or Y axes in ways that would improve loading resolution, and we load the full 3D volume instead.)
-      if (viewerState.viewMode === ViewMode.xy) {
-        const slice = viewerState.slice;
+      if (viewerStateRef.current.viewMode === ViewMode.xy) {
+        const slice = viewerStateRef.current.slice;
         requiredLoadSpec.subregion = new Box3(new Vector3(0, 0, slice.z), new Vector3(1, 1, slice.z));
       }
 
@@ -307,16 +317,8 @@ const useVolume = (scenePaths: (string | string[] | RawArrayLoaderOptions)[], op
       });
     };
     openImage();
-  }, [
-    sceneLoader,
-    getCurrentViewerChannelSettings,
-    onChannelDataLoaded,
-    onError,
-    placeImageInViewer,
-    setAllChannelsUnloaded,
-    setChannelStateForNewImage,
-    viewerState,
-  ]);
+  }, [sceneLoader, onError, viewerStateRef, channelVersionsRef, setChannelVersions, playControls]);
+  // of the above dependencies, we expect only `sceneLoader` to change.
 };
 
 export default useVolume;
