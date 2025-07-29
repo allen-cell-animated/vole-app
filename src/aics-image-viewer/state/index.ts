05c69011adbe4eb82ee28849c9e69f7e1906794f
Cameron Fraser <cameron.fraser@alleninstitute.org>
2025-07-29 14:30:06 -0700

    start experimenting with zustand

---
+++ src/aics-image-viewer/state/index.ts
@@ -0,0 +1,66 @@
+import { create } from "zustand";
+
+import { RenderMode, ViewMode } from "../shared/enums";
+
+import { ViewerState } from "../components/ViewerStateProvider/types.js";
+import { getDefaultViewerState } from "../shared/constants.js";
+
+// TODO move back to a new `types` module (?)
+type ViewerSettingChangeHandlers = {
+  [K in keyof ViewerState]?: (value: Partial<ViewerState[K]>, settings: ViewerState) => Partial<ViewerState>;
+};
+
+const isObject = <T>(val: T): val is Extract<T, Record<string, unknown>> =>
+  typeof val === "object" && val !== null && !Array.isArray(val);
+
+const VIEWER_SETTINGS_CHANGE_HANDLERS: ViewerSettingChangeHandlers = {
+  // Do not allow path trace render mode in 2D view modes
+  viewMode: (viewMode, { renderMode }) => {
+    const switchToVolumetric = viewMode !== ViewMode.threeD && renderMode === RenderMode.pathTrace;
+    return {
+      viewMode,
+      renderMode: switchToVolumetric ? RenderMode.volumetric : renderMode,
+    };
+  },
+
+  // Don't switch to path trace rendering in any view mode other than 3d; if we do switch, turn off autorotate
+  renderMode: (renderMode, { viewMode, autorotate }) => {
+    const willPathtrace = renderMode === RenderMode.pathTrace;
+    if (willPathtrace && viewMode === ViewMode.threeD) {
+      return {};
+    }
+
+    return {
+      renderMode,
+      autorotate: autorotate && !willPathtrace,
+    };
+  },
+
+  // Do not allow autorotate while in pathtrace mode (button should be disabled, but this provides extra security)
+  autorotate: (autorotate, { renderMode }) => ({
+    autorotate: autorotate && renderMode !== RenderMode.pathTrace,
+  }),
+};
+
+type ViewerStateActions = {
+  changeViewerSetting: <K extends keyof ViewerState>(key: K, value: ViewerState[K]) => void;
+};
+
+type ViewerStore = ViewerState & ViewerStateActions;
+
+export const useViewerState = create<ViewerStore>((set) => ({
+  ...getDefaultViewerState(),
+  changeViewerSetting: <K extends keyof ViewerState>(key: K, value: ViewerState[K]) => {
+    let changeHandler = VIEWER_SETTINGS_CHANGE_HANDLERS[key];
+
+    if (changeHandler) {
+      set((state) => changeHandler(value, state));
+    } else {
+      set((state) => {
+        let currentValue = state[key];
+        let nextValue = isObject(currentValue) && isObject(value) ? { ...currentValue, ...value } : value;
+        return { [key]: nextValue };
+      });
+    }
+  },
+}));
