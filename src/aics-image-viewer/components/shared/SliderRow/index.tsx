f6672a92070e71197eae8dd2aacc7450490bdf72
Cameron Fraser <cameron.fraser@alleninstitute.org>
2025-09-22 09:18:58 -0700

    rework `SliderRow` behavior a bit

--- src/aics-image-viewer/components/shared/SliderRow/index.tsx
+++ src/aics-image-viewer/components/shared/SliderRow/index.tsx
@@ -26,20 +26,19 @@ const SliderRow: React.FC<SliderRowProps> = (props) => (
   <div className="viewer-control-row">
     <div className="control-name">{props.label}</div>
     <div className="control">
-      {props.start === undefined
-        ? props.children
-        : !props.hideSlider && (
-            <SmarterSlider
-              range={{ min: props.min ?? 0, max: props.max }}
-              start={props.start}
-              connect={true}
-              tooltips={true}
-              behaviour="drag"
-              format={props.formatInteger ? INTEGER_FORMATTER : undefined}
-              onUpdate={props.onUpdate}
-              onChange={props.onChange}
-            />
-          )}
+      {props.start !== undefined && !props.hideSlider && (
+        <SmarterSlider
+          range={{ min: props.min ?? 0, max: props.max }}
+          start={props.start}
+          connect={true}
+          tooltips={true}
+          behaviour="drag"
+          format={props.formatInteger ? INTEGER_FORMATTER : undefined}
+          onUpdate={props.onUpdate}
+          onChange={props.onChange}
+        />
+      )}
+      {props.children}
     </div>
   </div>
 );
