8e2d57a47766a386f6fe08cfed184abb23b30b5c
Peyton Lee <peyton.lee@alleninstitute.org>
2025-12-04 15:28:16 -0800

    refactor: Use vole-core `v4.5.0`

--- website/utils/urls.ts
+++ website/utils/urls.ts
@@ -1,11 +1,7 @@
 import type { MultisceneUrls } from "../../src/aics-image-viewer/components/App/types";
 
-const S3_URL_PREFIX = "s3://";
-const GCS_URL_PREFIX = "gs://";
-const HTTP_REGEX = /^https?:\/\//i;
-
 export function isValidUrl(url: string): boolean {
-  return HTTP_REGEX.test(url) || url.startsWith(S3_URL_PREFIX) || url.startsWith(GCS_URL_PREFIX);
+  return url.startsWith("http");
 }
 
 export function encodeImageUrlProp(imageUrl: string | MultisceneUrls): string {
@@ -15,31 +11,4 @@ export function encodeImageUrlProp(imageUrl: string | MultisceneUrls): string {
   const sceneUrls = scenes.map((scene) => encodeURIComponent(Array.isArray(scene) ? scene.join(",") : scene));
   // join scenes with `+`
   return sceneUrls.join("+");
-}
-
-export function remapUrl(url: string): string {
-  url = url.trim();
-  if (url.startsWith(S3_URL_PREFIX)) {
-    // remap s3://bucket/key to https://bucket.s3.amazonaws.com/key
-    const s3Path = url.slice(S3_URL_PREFIX.length);
-    const pathSegments = s3Path.split("/");
-    url = `https://${pathSegments[0]}.s3.amazonaws.com/${pathSegments.slice(1).join("/")}`;
-  } else if (url.startsWith(GCS_URL_PREFIX)) {
-    // remap gs://bucket/key to https://storage.googleapis.com/bucket/key
-    url = url.replace(GCS_URL_PREFIX, "https://storage.googleapis.com/");
-  }
-  return url;
-}
-
-/**
- * Remaps S3 (s3://) and Google Cloud Storage (gs://) URLs to HTTPS URLs.
- */
-export function remapMultisceneUrls(imageUrls: MultisceneUrls["scenes"]): MultisceneUrls["scenes"] {
-  return imageUrls.map((scene) => {
-    if (Array.isArray(scene)) {
-      return scene.map(remapUrl);
-    } else {
-      return remapUrl(scene);
-    }
-  });
 }
