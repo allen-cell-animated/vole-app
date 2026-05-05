0fa68a02945160549a233fc0e0587e78dbb35338
Cameron Fraser <cameron.fraser@alleninstitute.org>
2026-05-05 15:01:39 -0700

    get a slider component going

--- src/aics-image-viewer/components/RotationControls.tsx
+++ src/aics-image-viewer/components/RotationControls.tsx
@@ -3,12 +3,39 @@ import React from "react";
 
 import SliderRow from "./shared/SliderRow";
 
+type RotationSliderProps = {
+  label: string;
+  onChange: (delta: number) => void;
+};
+
 export type RotationControlsProps = {
   view3d: View3d;
 };
 
+const RotationSlider: React.FC<RotationSliderProps> = ({ label, onChange }) => {
+  const [rotationDelta, setRotationDelta] = React.useState(0);
+
+  const onUpdate = React.useCallback(
+    ([value]: number[]) => {
+      setRotationDelta((prev) => {
+        onChange(value - prev);
+        return value;
+      });
+    },
+    [onChange]
+  );
+
+  const onRelease = React.useCallback(() => setRotationDelta(0), []);
+
+  return <SliderRow label={label} min={-90} max={90} start={rotationDelta} onUpdate={onUpdate} onChange={onRelease} />;
+};
+
 const RotationControls: React.FC<RotationControlsProps> = ({ view3d }) => {
-  return <SliderRow label="test" />;
+  return (
+    <div style={{ padding: "18px 16px 22px" }}>
+      <RotationSlider label="X" onChange={console.log} />
+    </div>
+  );
 };
 
 export default RotationControls;
