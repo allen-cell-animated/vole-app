24275cadbad2c8e6647f25911dae9bdc9c79bcfc
Cameron Fraser <cameron.fraser@alleninstitute.org>
2026-05-05 23:07:48 -0700

    get horizontal rotation going

--- src/aics-image-viewer/components/RotationControls.tsx
+++ src/aics-image-viewer/components/RotationControls.tsx
@@ -20,40 +20,52 @@ const toRadians = (deg: number): number => deg * (Math.PI / 180);
 
 const length = ([x, y, z]: Tuple3): number => Math.sqrt(x * x + y * y + z * z);
 
+const add = ([ax, ay, az]: Tuple3, [bx, by, bz]: Tuple3): Tuple3 => [ax + bx, ay + by, az + bz];
+const sub = ([ax, ay, az]: Tuple3, [bx, by, bz]: Tuple3): Tuple3 => [ax - bx, ay - by, az - bz];
+const mulScalar = ([x, y, z]: Tuple3, n: number): Tuple3 => [x * n, y * n, z * n];
+
 const cross = ([ax, ay, az]: Tuple3, [bx, by, bz]: Tuple3): Tuple3 => {
   return [ay * bz - by * az, az * bx - bz * ax, ax * by - bx * ay];
 };
 
 const normalize = (vec: Tuple3): Tuple3 => {
   const len = length(vec);
-  const [x, y, z] = vec;
-  return [x / len, y / len, z / len];
+  return mulScalar(vec, 1.0 / len);
 };
 
-const vecToTarget = (state: CameraState): Tuple3 => {
-  const [xPos, yPos, zPos] = state.position;
-  const [xTarget, yTarget, zTarget] = state.target;
-  return [xTarget - xPos, yTarget - yPos, zTarget - zPos];
+const vecToTarget = (state: CameraState): Tuple3 => sub(state.target, state.position);
+
+const rotateHorizontal = (state: CameraState, deg: number): Partial<CameraState> => {
+  const rad = toRadians(deg);
+  const toTarget = vecToTarget(state);
+  const forward = normalize(toTarget);
+  const right = cross(forward, state.up);
+  const nextForward = add(mulScalar(forward, Math.cos(rad)), mulScalar(right, -Math.sin(rad)));
+  const distance = length(toTarget);
+  const position = sub(state.target, mulScalar(nextForward, distance));
+  return { position };
 };
 
-// const rotateX = (state: CameraState, deltaX: number): Partial<CameraState> => {
-//   const toTarget = vecToTarget(state);
-//   const forward = normalize(toTarget);
-//   const [xRight, yRight, zRight] = cross(forward, state.up);
-//   const [xForward, yForward, zForward] = forward;
-// };
+const useCameraCallback = <T extends any[]>(
+  transform: (state: CameraState, ...args: T) => Partial<CameraState>,
+  view3d: View3d
+): ((...args: T) => void) => {
+  return React.useCallback(
+    (...args: T) => {
+      const state = view3d.getCameraState();
+      const nextState = transform(state, ...args);
+      view3d.setCameraState(nextState);
+    },
+    [view3d, transform]
+  );
+};
 
 const useCameraJumpCallback = (getPosition: (dist: number) => Tuple3, view3d: View3d, yUp = false): (() => void) => {
-  return React.useCallback(() => {
-    const state = view3d.getCameraState();
+  return useCameraCallback((state) => {
     const dist = length(vecToTarget(state));
-    const [xPos, yPos, zPos] = getPosition(dist);
-    const [xTarget, yTarget, zTarget] = state.target;
-    view3d.setCameraState({
-      position: [xTarget + xPos, yTarget + yPos, zTarget + zPos],
-      up: yUp ? [0, 1, 0] : [1, 0, 0],
-    });
-  }, [view3d, yUp, getPosition]);
+    const position = add(getPosition(dist), state.target);
+    return { position, up: yUp ? [0, 1, 0] : [1, 0, 0] };
+  }, view3d);
 };
 
 type VectorFn = (len: number) => Tuple3;
@@ -79,12 +91,12 @@ const RotationSlider: React.FC<{ label: string; onChange: (delta: number) => voi
 
   const onRelease = React.useCallback(() => setDelta(0), []);
 
-  return (
-    <SliderRow label={label} min={-90} max={90} start={delta} onUpdate={onUpdate} onChange={onRelease} pips={PIPS} />
-  );
+  return <SliderRow label={label} min={-90} max={90} start={delta} onUpdate={onUpdate} onChange={onRelease} />;
 };
 
 const RotationControls: React.FC<RotationControlsProps> = ({ view3d }) => {
+  const rotateHorizontalCallback = useCameraCallback(rotateHorizontal, view3d);
+
   const jumpXMinus = useCameraJumpCallback(X_MINUS, view3d, true);
   const jumpXPlus = useCameraJumpCallback(X_PLUS, view3d, true);
   const jumpYMinus = useCameraJumpCallback(Y_MINUS, view3d);
@@ -102,7 +114,7 @@ const RotationControls: React.FC<RotationControlsProps> = ({ view3d }) => {
         <Button onClick={jumpZMinus}>-Z</Button>
         <Button onClick={jumpZPlus}>+Z</Button>
       </SliderRow>
-      <RotationSlider label="X" onChange={console.log} />
+      <RotationSlider label="horizontal" onChange={rotateHorizontalCallback} />
     </div>
   );
 };
