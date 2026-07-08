6000e9fb56c0f261322e2433231feef8e2a7532e
Cameron Fraser <cameron.fraser@alleninstitute.org>
2026-07-08 11:43:05 -0700

    simplify clipboard permission queries

--- src/aics-image-viewer/components/ControlPanel/CopySettingsButton.tsx
+++ src/aics-image-viewer/components/ControlPanel/CopySettingsButton.tsx
@@ -11,28 +11,13 @@ import { useViewerState } from "../../state/store";
 
 import { useContextualAlert } from "../shared/ContextualAlert";
 
-const enum ClipboardState {
-  /** Pasting is enabled: we either know the clipboard contains paste-able settings or we can't be sure it doesn't */
-  Enabled,
-  /** Pasting is disabled because the clipboard doesn't contain paste-able settings */
-  Invalid,
-  /** Copying and pasting are both disabled because the browser is denying us access to the clipboard */
-  Denied,
-}
-
-const clipboardStatePrompts = {
-  [ClipboardState.Enabled]: undefined,
-  [ClipboardState.Invalid]: "Can't paste: clipboard does not contain channel settings.",
-  [ClipboardState.Denied]: "Can't paste: you must grant permission to access the clipboard.",
-};
-
 const CopySettingsButton: React.FC<{ scrollContainer?: HTMLElement | null; hide?: boolean }> = ({
   scrollContainer,
   hide,
 }) => {
-  const [clipboardState, setClipboardState] = React.useState(ClipboardState.Enabled);
+  const [pasteDenied, setPasteDenied] = React.useState(false);
   const buttonRef = React.useRef<HTMLButtonElement>(null);
-  const [alert, showMessage] = useContextualAlert(buttonRef.current, { scrollContainer, hide, timeout: 3_600_000 });
+  const [alert, showMessage] = useContextualAlert(buttonRef.current, { scrollContainer, hide, timeout: 8_000 });
 
   /** Learn as much as we can about whether the "paste" action will succeed, so we can disable it in advance. */
   const queryPasteState = React.useCallback(async (): Promise<void> => {
@@ -40,28 +25,11 @@ const CopySettingsButton: React.FC<{ scrollContainer?: HTMLElement | null; hide?
     try {
       // Chromium browsers: we can query permissions to learn whether a clipboard read will succeed
       permission = await navigator.permissions.query({ name: "clipboard-read" as PermissionName });
+      // If we're here, the `clipboard-read` permission is supported. Its state may be `denied`, `granted`, or `prompt`.
+      setPasteDenied(permission.state === "denied");
     } catch {
       // Non-Chromium browsers: clipboard reads don't have stateful permissions and will always require a prompt
-      setClipboardState(ClipboardState.Enabled);
-      return;
-    }
-
-    // If we're here, the `clipboard-read` permission is supported. Its value may be `denied`, `granted`, or `prompt`.
-    if (permission.state === "denied") {
-      setClipboardState(ClipboardState.Denied);
-    } else if (permission.state === "granted") {
-      try {
-        const clipboardText = await navigator.clipboard.readText();
-        const clipboardJson = JSON.parse(clipboardText);
-        if (isClipboardChannelState(clipboardJson)) {
-          setClipboardState(ClipboardState.Enabled);
-        } else {
-          setClipboardState(ClipboardState.Invalid);
-        }
-      } catch {
-        // Clipboard does not contain JSON
-        setClipboardState(ClipboardState.Invalid);
-      }
+      setPasteDenied(false);
     }
   }, []);
 
@@ -73,8 +41,8 @@ const CopySettingsButton: React.FC<{ scrollContainer?: HTMLElement | null; hide?
     return () => navigator.clipboard.removeEventListener("clipboardchange", queryPasteState);
   }, [queryPasteState]);
 
-  const pastePrompt = clipboardStatePrompts[clipboardState] && (
-    <Tooltip title={clipboardStatePrompts[clipboardState]} placement="right">
+  const pastePrompt = pasteDenied && (
+    <Tooltip title="You must grant access to the clipboard" placement="right">
       <ExclamationCircleOutlined />
     </Tooltip>
   );
@@ -102,7 +70,7 @@ const CopySettingsButton: React.FC<{ scrollContainer?: HTMLElement | null; hide?
           {pastePrompt}
         </div>
       ),
-      disabled: clipboardState !== ClipboardState.Enabled,
+      disabled: pasteDenied,
       onClick: async () => {
         const { channelSettings, replaceAllChannelSettings } = useViewerState.getState();
         try {
@@ -121,7 +89,7 @@ const CopySettingsButton: React.FC<{ scrollContainer?: HTMLElement | null; hide?
           showMessage(
             <>
               Settings applied -{" "}
-              <Button type="link" style={{ padding: 0 }}>
+              <Button type="link" style={{ padding: 0, height: "unset" }}>
                 Undo
               </Button>
             </>
