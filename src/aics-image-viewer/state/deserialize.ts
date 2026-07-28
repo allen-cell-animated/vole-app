ebd7df28e5ad7302ed8d4d069251b68ed0afbce9
Cameron Fraser <cameron.fraser@alleninstitute.org>
2026-07-28 15:33:03 -0700

    shrink an `if`

--- src/aics-image-viewer/state/deserialize.ts
+++ src/aics-image-viewer/state/deserialize.ts
@@ -451,13 +451,11 @@ export function deserializeChannelState(
     result.ramp = controlPointsToRamp(pointsFromLut);
   }
 
-  if (jsonState[ViewerChannelSettingKeys.ControlPoints]) {
-    const parsedResult = parseControlPoints(jsonState[ViewerChannelSettingKeys.ControlPoints]);
-    if (parsedResult) {
-      result.controlPoints = parsedResult;
-    }
-  } else if (jsonState[ViewerChannelSettingKeys.ControlPointsLegacy]) {
-    const parsedResult = parseControlPoints(jsonState[ViewerChannelSettingKeys.ControlPointsLegacy]);
+  const controlPointsSerialized =
+    jsonState[ViewerChannelSettingKeys.ControlPoints] ?? jsonState[ViewerChannelSettingKeys.ControlPointsLegacy];
+
+  if (controlPointsSerialized) {
+    const parsedResult = parseControlPoints(controlPointsSerialized);
     if (parsedResult) {
       result.controlPoints = parsedResult;
     }
