d9c07e1eeda4875ab3066ecaa4243436bbc0eeb7
Peyton Lee <peyton.lee@alleninstitute.org>
2026-09-03 16:21:17 -0700

    fix: Split src and website constants

--- website/components/LandingPage/index.tsx
+++ website/components/LandingPage/index.tsx
@@ -5,9 +5,8 @@ import { useSearchParams } from "react-router-dom";
 import styled from "styled-components";
 
 import { parseViewerUrlParams } from "../../../src";
-import { VOLEAPP_BUILD_ENVIRONMENT } from "../../../src/constants";
 import { BannerVideo } from "../../assets/videos";
-import { GTM_ID } from "../../constants";
+import { GTM_ID, VOLEAPP_BUILD_ENVIRONMENT } from "../../constants";
 import type { AppDataProps } from "../../types";
 import { encodeImageUrlProp } from "../../utils/urls";
 import { LANDING_PAGE_CONTENT } from "./content";
