9df8d675f3e0417b10f059e8356fa5e218ecb067
Peyton Lee <peyton.lee@alleninstitute.org>
2026-09-03 15:21:02 -0700

    refactor: Update environment constant declaration

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const VOLEAPP_VERSION = import.meta.env.VITE_VOLEAPP_VERSION;
+export const VOLECORE_VERSION = import.meta.env.VITE_VOLECORE_VERSION;
+export const VOLEAPP_BUILD_ENVIRONMENT = import.meta.env.VITE_BUILD_ENVIRONMENT;
+export const VOLEAPP_BASENAME = import.meta.env.VITE_BASENAME;
