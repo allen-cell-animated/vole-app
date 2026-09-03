f2d57c07e2988311a53de3102c9dad8ecd471e39
Peyton Lee <peyton.lee@alleninstitute.org>
2026-09-03 14:56:06 -0700

    refactor: Installed vite, initial attempt at vite config

---
+++ vite.config.js
@@ -0,0 +1,71 @@
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import glsl from "vite-plugin-glsl";
+
+const voleCorePackagePath = require.resolve("@aics/vole-core/package.json", { paths: [__dirname] });
+
+// Add version number and build timestamp to the environment variables
+const DEFAULT_ENV = {
+  ...process.env,
+  // VITE_APP_VERSION: process.env.npm_package_version,
+  // VITE_BUILD_TIME_UTC: Date.now().toString(),
+  VOLEAPP_VERSION: JSON.stringify(require("./package.json").version),
+  VOLECORE_VERSION: JSON.stringify(require(voleCorePackagePath).version),
+  VOLEAPP_BUILD_ENVIRONMENT: JSON.stringify(process.env.env),
+  VOLEAPP_BASENAME: JSON.stringify(process.env.basename),
+};
+
+process.env = DEFAULT_ENV;
+
+const DEFAULT_CONFIG = {
+  build: {
+    // This quiets the "module has been externalized for browser compatibility" warnings that
+    // vite throws when building for production.
+    // commonjsOptions: {
+    //   // Ignore built-in modules in Node.js. Fix copied from the workerpool documentation:
+    //   // https://github.com/josdejong/workerpool/blob/master/examples/vite/vite.config.ts
+    //   ignore: ["os", "child_process", "worker_threads"],
+    // },
+    rollupOptions: {
+      input: {
+        // TODO: Currently 404.html imports all of the same JS chunks as index.html.
+        // Can we make it so that 404.html only imports `gh_404.js` chunks?
+
+        // eslint-disable-next-line no-undef
+        main: resolve(__dirname, "./public/index.html"),
+        // eslint-disable-next-line no-undef
+        404: resolve(__dirname, "./public/404.html"),
+      },
+      output: {
+        dir: resolve(__dirname, "./imageviewer"),
+      },
+    },
+  },
+  optimizeDeps: {
+    // vole-core uses a worker imported via local import (e.g. new
+    // URL('./local-worker.js', import.meta.url)). If vite bundles the worker,
+    // vole-core will not be able to find it at runtime. We exclude vole-core
+    // from dependency optimization here.
+    exclude: ["@aics/vole-core"],
+    // Have to still optimize all CommonJS dependencies of vole-core. See
+    // https://vite.dev/config/dep-optimization-options#optimizedeps-exclude
+    include: [
+      "@aics/vole-core > tweakpane",
+      "@aics/vole-core > geotiff",
+      "@aics/vole-core > numcodecs",
+      "@aics/vole-core > throttled-queue",
+    ],
+  },
+  resolve: {
+    alias: [{ find: "src", replacement: resolve(__dirname, "./src") }],
+  },
+  worker: {
+    // Fixes an invalid output format error when building
+    format: "es",
+  },
+  // plugins: [svgr(), glsl(), react()],
+  plugins: [glsl(), react()],
+};
+
+module.exports = defineConfig(DEFAULT_CONFIG);
