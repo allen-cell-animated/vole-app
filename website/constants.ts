12e193b03c5e4aefc05b69e65d51069c263d5398
Peyton Lee <peyton.lee@alleninstitute.org>
2026-09-04 14:37:51 -0700

    refactor: More build fixes

--- website/constants.ts
+++ website/constants.ts
@@ -1,5 +1,5 @@
 export const VOLEAPP_VERSION: string = JSON.parse(import.meta.env.VITE_VOLEAPP_VERSION);
 export const VOLECORE_VERSION: string = JSON.parse(import.meta.env.VITE_VOLECORE_VERSION);
 export const VOLEAPP_BUILD_ENVIRONMENT: "production" | "dev" =
-  JSON.parse(import.meta.env.MODE) === "production" ? "production" : "dev";
+  import.meta.env.MODE === "production" ? "production" : "dev";
 export const VOLEAPP_BASENAME: string | undefined = import.meta.env.VITE_BASENAME;
