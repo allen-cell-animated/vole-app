107e58dd9f303d688ced7b92fc0fd8eabf39a2bc
Peyton Lee <peyton.lee@alleninstitute.org>
2026-09-04 17:29:21 -0700

    fix: Basenames from env

--- website/vite-env.d.ts
+++ website/vite-env.d.ts
@@ -4,5 +4,4 @@ interface ImportMetaEnv {
   readonly VITE_VOLEAPP_VERSION: string;
   readonly VITE_VOLECORE_VERSION: string;
   readonly VITE_BUILD_TIME_UTC: string;
-  readonly VITE_BASENAME: string;
 }
