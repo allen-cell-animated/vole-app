738ba2b3aa16b376331ad25a04a6c7745eab81e6
Peyton Lee <peyton.lee@alleninstitute.org>
2026-09-04 17:01:19 -0700

    refactor: Remove unused eslint rules

--- website/vite-env.d.ts
+++ website/vite-env.d.ts
@@ -1,12 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   readonly VITE_VOLEAPP_VERSION: string;
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   readonly VITE_VOLECORE_VERSION: string;
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   readonly VITE_BUILD_TIME_UTC: string;
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   readonly VITE_BASENAME: string;
 }
