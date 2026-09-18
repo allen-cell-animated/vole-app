e5d4132e47529ed9c80e126d78d4f436dcc5e2ba
Cameron Fraser <cameron.fraser@alleninstitute.org>
2026-09-18 16:38:58 -0700

    add and parse `import` param

--- website/components/AppWrapper.tsx
+++ website/components/AppWrapper.tsx
@@ -4,8 +4,19 @@ import React, { type ReactElement, useCallback, useEffect, useState } from "reac
 import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 
-import { ImageViewerApp, parseViewerUrlParams, viewerMessageToParams, writeMetadata, writeScenes } from "../../src";
+import {
+  ImageViewerApp,
+  parseViewerUrlParams,
+  snapshotToViewerState,
+  viewerMessageToParams,
+  writeMetadata,
+  writeScenes,
+} from "../../src";
 import { getDefaultViewerChannelSettings } from "../../src/aics-image-viewer/shared/constants";
+import {
+  isSessionSnapshot,
+  snapshotToViewerChannelSettings,
+} from "../../src/aics-image-viewer/shared/utils/parseSnapshot";
 import { select, useViewerState } from "../../src/aics-image-viewer/state/store";
 import type { ViewerState } from "../../src/aics-image-viewer/state/types";
 import type { AppDataProps } from "../types";
@@ -20,6 +31,7 @@ import ShareModal from "./Modals/ShareModal";
 
 const MSG_ORIGIN_PARAM = "msgorigin";
 const COLLECTION_ID_PARAM = "collectionid";
+const IMPORT_PARAM = "import";
 
 const DEFAULT_APP_PROPS: AppDataProps = {
   imageUrl: "",
@@ -73,21 +85,52 @@ export default function AppWrapper(props: AppWrapperProps): ReactElement {
     let ignore = false;
 
     const getViewerStateFromSearchParams = async (): Promise<void> => {
+      let receivedProps: Partial<AppDataProps> = {};
+      let receivedViewerSettings: Partial<ViewerState> | undefined = undefined;
+
+      // Parse state from viewer URL params
       try {
         const urlArgs = await parseViewerUrlParams(window.location.search);
         if (ignore) return;
-        setViewerProps({ ...DEFAULT_APP_PROPS, ...urlArgs.args, ...locationArgs });
-
-        const viewerSettings = { ...urlArgs.viewerSettings, ...locationArgs?.viewerSettings };
-        if (viewerSettings && !isEqual(viewerSettings, prevViewerSettingsRef.current)) {
-          mergeViewerSettings(viewerSettings);
-          prevViewerSettingsRef.current = viewerSettings;
-        }
+        receivedProps = { ...receivedProps, ...urlArgs.args };
+        receivedViewerSettings = { ...(receivedViewerSettings ?? {}), ...urlArgs.viewerSettings };
       } catch (reason) {
         if (ignore) return;
         showErrorAlert("Failed to parse URL parameters: " + reason);
-        setViewerProps({ ...DEFAULT_APP_PROPS, ...locationArgs });
       }
+
+      // If the URL params point to a JSON state snapshot, fetch and parse it too
+      const jsonImportUrl = searchParams.get(IMPORT_PARAM);
+      if (jsonImportUrl !== null) {
+        try {
+          const response = await fetch(jsonImportUrl);
+          const jsonImportText = await response.text();
+          if (ignore) return;
+          const jsonImport = JSON.parse(jsonImportText);
+          if (isSessionSnapshot(jsonImport)) {
+            receivedProps = {
+              ...receivedProps,
+              ...viewerMessageToParams(jsonImport),
+              viewerChannelSettings: snapshotToViewerChannelSettings(jsonImport),
+            };
+            receivedViewerSettings = { ...(receivedViewerSettings ?? {}), ...snapshotToViewerState(jsonImport) };
+          }
+        } catch {
+          if (ignore) return;
+          // import invalid! TODO error message here
+        }
+      }
+
+      // Merge params from above
+      const resultViewerSettings = receivedViewerSettings && {
+        ...receivedViewerSettings,
+        ...locationArgs?.viewerSettings,
+      };
+      if (resultViewerSettings !== undefined && !isEqual(resultViewerSettings, prevViewerSettingsRef.current)) {
+        mergeViewerSettings(resultViewerSettings);
+        prevViewerSettingsRef.current = resultViewerSettings;
+      }
+      setViewerProps({ ...DEFAULT_APP_PROPS, ...receivedProps, ...locationArgs });
     };
 
     // Handle the opening window wanting to send more data via a message
